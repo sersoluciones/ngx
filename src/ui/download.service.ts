deecdb97ad318dc0e9bedfbef55c807bc38eba71
Andres Gamba <xnovaerax@gmail.com>
2021-06-04 14:25:00 -0500

    Fix download

--- src/ui/download.service.ts
+++ src/ui/download.service.ts
@@ -10,16 +10,16 @@ export class DownloadService {
     constructor() {
         this.downloadElement = document.createElement('a');
         this.downloadElement.setAttribute('target', '_blank');
-        document.body.appendChild(this.downloadElement);
     }
 
     /**
      * @description Descarga tradicional mediante un a[href] emulando un clic de usuario
      * @param url Url absoluta
      */
-    get(url: string) {
+    get(url: string, filename?: string) {
+        document.body.appendChild(this.downloadElement);
         this.downloadElement.setAttribute('href', url);
-        this.downloadElement.setAttribute('download', url.split('/').pop());
+        this.downloadElement.setAttribute('download', filename ? filename : url.split('/').pop());
         this.downloadElement.click();
         document.body.removeChild(this.downloadElement);
     }
